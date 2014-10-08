c9af7f511b9987c7e74e989fcfa17345355dbf1e
Márcio Duarte <maethorin@gmail.com>
2014-10-08 14:41:17 -0300

    Versionando pacotes para o pypi

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setup(
     name='li-pagador-pagseguro',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/lojaintegrada/LI-Pagador-PagSeguro',
     license='MIT',
     description='Meio de pagamento usando o PagSeguro (https://pagseguro.uol.com.br)',
