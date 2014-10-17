ecc372837ea90a9f3eb44ac9f1634a5356f264f1
Márcio Duarte <maethorin@gmail.com>
2014-10-17 15:35:29 -0300

    Versionando

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setup(
     name='li-pagador-pagseguro',
-    version='0.0.8',
+    version='0.0.9',
     url='https://github.com/lojaintegrada/LI-Pagador-PagSeguro',
     license='MIT',
     description='Meio de pagamento usando o PagSeguro (https://pagseguro.uol.com.br)',
