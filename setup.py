c4a73b4d159cec7bf950eee997eb64fd9fee5916
Udlei Nattis <udlei@nati.biz>
2014-10-18 13:02:41 -0300

    nova versão

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setup(
     name='li-pagador-pagseguro',
-    version='0.0.14',
+    version='0.0.15',
     url='https://github.com/lojaintegrada/LI-Pagador-PagSeguro',
     license='MIT',
     description='Meio de pagamento usando o PagSeguro (https://pagseguro.uol.com.br)',
