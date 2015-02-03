f350811a70260b0e45a45883a0eced51d03059d2
Jeferson Farias Calazans <calazans10@gmail.com>
2015-02-03 15:44:24 -0200

    Atualizando nome dos templates

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setup(
     name='li-pagador-pagseguro',
-    version='1.0.4',
+    version='1.0.5',
     url='https://github.com/lojaintegrada/LI-Pagador-PagSeguro',
     license='MIT',
     description='Meio de pagamento usando o PagSeguro (https://pagseguro.uol.com.br)',
