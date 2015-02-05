10ad78155d01ff62a26910093f8b6c2f3c216f1d
Jeferson Farias Calazans <calazans10@gmail.com>
2015-02-05 11:49:04 -0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setup(
     name='li-pagador-pagseguro',
-    version='1.0.5',
+    version='1.0.6',
     url='https://github.com/lojaintegrada/LI-Pagador-PagSeguro',
     license='MIT',
     description='Meio de pagamento usando o PagSeguro (https://pagseguro.uol.com.br)',
