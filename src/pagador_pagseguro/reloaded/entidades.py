826662915732c69e94241e773d4a31c6c74f59c9
maethorin <maethorin@gmail.com>
2015-02-27 23:23:13 -0300

    código do gateway como constante

--- src/pagador_pagseguro/reloaded/entidades.py
+++ src/pagador_pagseguro/reloaded/entidades.py
@@ -3,10 +3,12 @@
 from pagador.reloaded import entidades
 from pagador_pagseguro.reloaded import cadastro
 
+CODIGO_GATEWAY = 1
+
 
 class ConfiguracaoMeioPagamento(entidades.ConfiguracaoMeioPagamento):
     _campos = ['ativo', 'aplicacao', 'codigo_autorizacao', 'valor_minimo_aceitado', 'valor_minimo_parcela', 'mostrar_parcelamento', 'maximo_parcelas', 'parcelas_sem_juros']
-    _codigo_gateway = 1
+    _codigo_gateway = CODIGO_GATEWAY
 
     def __init__(self, loja_id, codigo_pagamento=None):
         super(ConfiguracaoMeioPagamento, self).__init__(loja_id, codigo_pagamento)
