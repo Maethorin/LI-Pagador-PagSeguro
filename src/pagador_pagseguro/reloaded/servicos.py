62100d25fbe2f64593e7015c16aa5ca8635b22fb
maethorin <maethorin@gmail.com>
2015-03-04 13:22:06 -0300

    Mega refactor em nomes parte 1

--- src/pagador_pagseguro/reloaded/servicos.py
+++ src/pagador_pagseguro/reloaded/servicos.py
@@ -67,4 +67,123 @@ class InstalaMeioDePagamento(servicos.InstalaMeioDePagamento):
         raise self.InstalacaoNaoFinalizada(u'Erro ao entrar em contato com o PagSeguro. Código: {} - Resposta: {}'.format(resposta.status_code, resposta.conteudo))
 
     def desinstalar(self, dados):
-        return {"redirect": "https://{}pagseguro.uol.com.br/aplicacao/listarAutorizacoes.jhtml".format(self.sandbox)}
+        return {'redirect': 'https://{}pagseguro.uol.com.br/aplicacao/listarAutorizacoes.jhtml'.format(self.sandbox)}
+
+
+class Credenciador(servicos.Credenciador):
+    def __init__(self, tipo=None, configuracao=None):
+        super(Credenciador, self).__init__(tipo, configuracao)
+        self.tipo = self.TipoAutenticacao.query_string
+        self.codigo_autorizacao = str(getattr(self.configuracao, 'codigo_autorizacao', ''))
+        self.chave = 'authorizationCode'
+
+    def obter_credenciais(self):
+        return self.codigo_autorizacao
+
+
+class EntregaPagamento(servicos.EntregaPagamento):
+    def __init__(self, loja_id, plano_indice=1):
+        super(EntregaPagamento, self).__init__(loja_id, plano_indice)
+        self.tem_malote = True
+        self.faz_http = True
+        self.conexao = self.obter_conexao(formato_envio=requisicao.Formato.form_urlencode, formato_resposta=requisicao.Formato.xml)
+        self.resposta_pagseguro = None
+        self.url = 'https://ws.{}pagseguro.uol.com.br/v2/checkout'.format(self.sandbox)
+
+    def define_credenciais(self):
+        self.conexao.credenciador = Credenciador(configuracao=self.configuracao)
+
+    def envia_pagamento(self, tentativa=1):
+        self.resposta_pagseguro = self.conexao.post(self.url, self.malote.to_dict())
+
+    def processa_dados_de_pagamento(self):
+        self.resultado = self._processa_resposta()
+
+    def _processa_resposta(self):
+        status_code = self.resposta_pagseguro.status_code
+        if self.resposta_pagseguro.erro_servidor:
+            return {'mensagem': u'O servidor do PagSeguro está indisponível nesse momento.', 'status_code': status_code}
+        if self.resposta_pagseguro.timeout:
+            return {'mensagem': u'O servidor do PagSeguro não respondeu em tempo útil.', 'status_code': status_code}
+        if self.resposta_pagseguro.nao_autenticado or self.resposta_pagseguro.nao_autorizado:
+            return {'mensagem': u'Autenticação da loja com o PagSeguro Falhou. Contate o SAC da loja.', 'status_code': status_code}
+        if self.resposta_pagseguro.sucesso:
+            url = 'https://{}pagseguro.uol.com.br/v2/checkout/payment.html?code={}'.format(self.sandbox, self.resposta_pagseguro.conteudo['checkout']['code'])
+            return {'url': url}
+        if 'errors' in self.resposta_pagseguro.conteudo:
+            erros = self.resposta_pagseguro.conteudo['errors']
+            mensagens = []
+            if type(erros) is list:
+                for erro in erros:
+                    mensagens.append('{} - {}'.format(erro['error']['code'], erro['error']['message']))
+            else:
+                mensagens.append('{} - {}'.format(erros['error']['code'], erros['error']['message']))
+            raise self.EnvioNaoRealizado(u'Ocorreram erros no envio dos dados para o PagSeguro', self.loja_id, self.pedido.numero, dados_envio=self.malote.to_dict(), erros=mensagens)
+
+
+class SituacoesDePagamento(object):
+    """
+    Traduz os códigos de status do PagSeguro em algo que o pagador entenda
+    Ver status disponíveis em:
+    https://pagseguro.uol.com.br/v2/guia-de-integracao/api-de-notificacoes.html
+    """
+    disponivel = '4'
+
+    DE_PARA = {
+        '1': servicos.SituacaoPedido.SITUACAO_AGUARDANDO_PAGTO,
+        '2': servicos.SituacaoPedido.SITUACAO_PAGTO_EM_ANALISE,
+        '3': servicos.SituacaoPedido.SITUACAO_PEDIDO_PAGO,
+        '5': servicos.SituacaoPedido.SITUACAO_PAGTO_EM_DISPUTA,
+        '6': servicos.SituacaoPedido.SITUACAO_PAGTO_DEVOLVIDO,
+        '7': servicos.SituacaoPedido.SITUACAO_PEDIDO_CANCELADO,
+        '8': servicos.SituacaoPedido.SITUACAO_PAGTO_CHARGEBACK
+    }
+
+    @classmethod
+    def do_tipo(cls, tipo):
+        return cls.DE_PARA.get(tipo, None)
+
+
+class RegistraResultado(servicos.RegistraResultado):
+    def __init__(self, loja_id, dados=None):
+        super(RegistraResultado, self).__init__(loja_id, dados)
+        self.conexao = self.obter_conexao(formato_envio=requisicao.Formato.querystring, formato_resposta=requisicao.Formato.xml)
+        self.resposta_pagseguro = None
+        self.redirect_para = dados.get('next_url', None)
+        self.faz_http = True
+
+    def define_credenciais(self):
+        self.conexao.credenciador = Credenciador(configuracao=self.configuracao)
+
+    def monta_dados_de_pagamento(self):
+        if self.obteve_dados_de_pagamento:
+            self.dados_de_pagamento['identificador_id'] = self.dados['transacao']
+            self.pedido_numero = self.dados["referencia"]
+            print self.resposta_pagseguro.conteudo
+            transacao = self.resposta_pagseguro.conteudo['transaction']
+            if 'code' in transacao:
+                self.dados_de_pagamento['transacao_id'] = transacao['code']
+            if 'grossAmount' in transacao:
+                self.dados_de_pagamento['valor_pago'] = transacao['grossAmount']
+            self.situacao_pedido = SituacoesDePagamento.do_tipo(transacao['status'])
+        self.resultado = {'resultado': 'OK'}
+
+    def obtem_informacoes_de_pagamento(self):
+        if self.deve_obter_informacoes_do_pagseguro:
+            aplicacao = 'pagseguro_alternativo' if self.configuracao.aplicacao == 'pagseguro_alternativo' else 'pagseguro'
+            parametros = self.cria_entidade_de_pagador('ParametrosDeContrato', loja_id=self.loja_id).obter_para(aplicacao)
+            dados = {
+                'appKey': parametros['app_secret'],
+                'appId': parametros['app_id'],
+            }
+            self.resposta_pagseguro = self.conexao.get(self.url, dados=dados)
+            self.obteve_dados_de_pagamento = self.resposta_pagseguro.sucesso
+
+    @property
+    def deve_obter_informacoes_do_pagseguro(self):
+        return 'transacao' in self.dados
+
+    @property
+    def url(self):
+        end_point = '' if 'transacao' in self.dados else '/notifications'
+        return 'https://ws.{}pagseguro.uol.com.br/v3/transactions{}/{}'.format(self.sandbox, end_point, self.dados['transacao'])
