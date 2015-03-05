e3bfe10c8e91a434fdfba8c237ab2c9d6c9226c5
maethorin <maethorin@gmail.com>
2015-03-05 17:58:30 -0300

    Passando dados para EntregaPagamento

--- src/pagador_pagseguro/reloaded/servicos.py
+++ src/pagador_pagseguro/reloaded/servicos.py
@@ -83,7 +83,7 @@ class Credenciador(servicos.Credenciador):
 
 class EntregaPagamento(servicos.EntregaPagamento):
     def __init__(self, loja_id, plano_indice=1, dados=None):
-        super(EntregaPagamento, self).__init__(loja_id, plano_indice, dados=None)
+        super(EntregaPagamento, self).__init__(loja_id, plano_indice, dados=dados)
         self.tem_malote = True
         self.faz_http = True
         self.conexao = self.obter_conexao(formato_envio=requisicao.Formato.form_urlencode, formato_resposta=requisicao.Formato.xml)
