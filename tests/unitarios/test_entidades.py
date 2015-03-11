20e8315e87fea695e7a32c905382a55de74a3912
Márcio Duarte <maethorin@gmail.com>
2015-03-11 18:16:22 -0300

    Testando entidades

--- tests/unitarios/test_entidades.py
+++ tests/unitarios/test_entidades.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from decimal import Decimal
 import unittest
 
 import mock
@@ -32,3 +33,41 @@ class PagSeguroConfiguracaoMeioPagamento(unittest.TestCase):
     def test_deve_ser_aplicacao(self, preencher_mock):
         configuracao = entidades.ConfiguracaoMeioPagamento(234)
         configuracao.eh_aplicacao.should.be.truthy
+
+
+class MontandoMalote(unittest.TestCase):
+    def test_deve_definir_tipo_de_envio_pac(self):
+        entidades.TipoEnvio('pac').valor.should.be.equal(1)
+
+    def test_deve_definir_tipo_de_envio_sedex(self):
+        entidades.TipoEnvio('sedex').valor.should.be.equal(2)
+
+    def test_deve_definir_tipo_de_envio_outro(self):
+        entidades.TipoEnvio('outro').valor.should.be.equal(3)
+
+    def test_malote_deve_ter_propriedades(self):
+        entidades.Malote('configuracao').to_dict().should.be.equal({'appId': None, 'appKey': None, 'currency': None, 'extraAmount': None, 'notificationURL': None, 'redirectURL': None, 'reference': None, 'senderAreaCode': None, 'senderEmail': None, 'senderName': None, 'senderPhone': None, 'shippingAddressCity': None, 'shippingAddressComplement': None, 'shippingAddressCountry': 'BRA', 'shippingAddressDistrict': None, 'shippingAddressNumber': None, 'shippingAddressPostalCode': None, 'shippingAddressState': None, 'shippingAddressStreet': None, 'shippingCost': None, 'shippingType': None})
+
+    def test_deve_montar_conteudo(self):
+        malote = entidades.Malote(mock.MagicMock(loja_id=8))
+        pedido = mock.MagicMock(
+            cliente_telefone=('21', '99999999'),
+            numero=1234,
+            cliente_nome_ascii='Nome',
+            forma_envio='pac',
+            cliente={'email': 'cliente@email.com'},
+            valor_envio=Decimal('14.00'),
+            valor_desconto=Decimal('4.00'),
+            endereco_entrega={
+                'nome': u'Nome endereço entrega', 'endereco': 'Rua entrega', 'numero': '51', 'complemento': 'lt 51',
+                'bairro': 'Bairro', 'cidade': 'Cidade', 'cep': '12908-212', 'estado': 'RJ'
+            },
+            itens=[
+                mock.MagicMock(nome='Produto 1', sku='PROD01', quantidade=1, preco_venda=Decimal('40.00'), url_produto='url-prd-1'),
+                mock.MagicMock(nome='Produto 2', sku='PROD02', quantidade=1, preco_venda=Decimal('50.00'), url_produto='url-prd-2'),
+            ]
+        )
+        dados = {'next_url': 'url-next'}
+        parametros = {'app_secret': 'app-secret', 'app_id': 'app-id'}
+        malote.monta_conteudo(pedido, parametros, dados)
+        malote.to_dict().should.be.equal({'appId': 'app-id', 'appKey': 'app-secret', 'currency': 'BRL', 'extraAmount': '-4.00', 'itemAmount1': '40.00', 'itemAmount2': '50.00', 'itemDescription1': 'Produto 1', 'itemDescription2': 'Produto 2', 'itemId1': 'PROD01', 'itemId2': 'PROD02', 'itemQuantity1': 1, 'itemQuantity2': 1, 'notificationURL': 'http://localhost:5000/pagador/meio-pagamento/pagseguro/retorno/8/notificacao', 'redirectURL': 'http://localhost:5000/pagador/meio-pagamento/pagseguro/retorno/8/resultado?next_url=url-next&referencia=1234', 'reference': 1234, 'senderAreaCode': '21', 'senderEmail': 'cliente@email.com', 'senderName': 'Nome', 'senderPhone': '99999999', 'shippingAddressCity': 'Cidade', 'shippingAddressComplement': 'lt 51', 'shippingAddressCountry': 'BRA', 'shippingAddressDistrict': 'Bairro', 'shippingAddressNumber': '51', 'shippingAddressPostalCode': '12908-212', 'shippingAddressState': 'RJ', 'shippingAddressStreet': 'Rua entrega', 'shippingCost': '14.00', 'shippingType': 1})
