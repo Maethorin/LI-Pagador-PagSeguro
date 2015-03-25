2f044c488bfad722f43c2a0d200142539dbc6aff
maethorin <maethorin@gmail.com>
2015-03-25 13:41:33 -0300

    Definindo modo de listagem na hora de criar entidade de gateway para evitar outro acesso ao banco

--- tests/integracao/test_acesso_via_api.py
+++ tests/integracao/test_acesso_via_api.py
@@ -21,7 +21,7 @@ class PagSeguroConfiguracaoMeioDePagamentoDaLoja(TestBase):
         response = self.app.get(self.url, follow_redirects=True, headers={'authorization': 'chave_aplicacao CHAVE-TESTE'})
         json.loads(response.data).should.be.equal({u'metadados': {u'api': u'API Pagador', u'resultado': u'sucesso', u'versao': u'1.0'}, u'sucesso': {u'configuracao_pagamento': u'PAGSEGURO'}})
         response.status_code.should.be.equal(200)
-        configuracao_mock.assert_called_with(loja_id=8, codigo_pagamento='pagseguro')
+        configuracao_mock.assert_called_with(loja_id=8, codigo_pagamento='pagseguro', eh_listagem=False)
 
     @mock.patch('pagador_pagseguro.entidades.ConfiguracaoMeioPagamento')
     def test_deve_grava_dados_pagseguro(self, configuracao_mock):
